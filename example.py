b672dcd20c9c0cb2e6d0baa1b4bd3f70d4c5ec24
GZX-ZJU <1013168797@qq.com>
2018-04-05 00:22:54 +0800

    object orient init

---
+++ example.py
@@ -0,0 +1,14 @@
+class One(object):
+	def __init__(self):
+		pass
+
+	def print_self(self):
+		print("one")
+
+
+class Two(object):
+	def __init__(self):
+		pass
+
+	def print_self(sefl):
+		print("two")
